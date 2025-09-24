20f38d692d3de3625496c9b27cf7287e8ddb0078
Christian <christian_96_09@hotmail.com>
2025-09-24 16:12:08 -0300

    actualiza la verificación de Google Search Console en layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -47,8 +47,8 @@ export default function RootLayout({
           `}
         </Script>
 
-        <meta name="google-site-verification" content="N2E2e6CttVrG1Y-reuUr9a-m-quaGIJ0K1yxihs_rq8" />
-
+        {/* Verificación de Google Search Console */}
+        <meta name="google-site-verification" content="l4rXnbvItyA44LlrCR-BH0aigDqlSrGupQWULWVVlts" />
 
         <meta charSet="utf-8" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"/>
