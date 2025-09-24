5acef858408cfe71166c51b436cca7378a3fc906
Christian <christian_96_09@hotmail.com>
2025-09-24 16:27:45 -0300

    agrega soporte para next-sitemap y configura sitemaps y robots.txt

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://crisfide.vercel.app/', // reemplazá con tu dominio
+    generateRobotsTxt: true, // genera también robots.txt
+    // sitemapSize: 5000, // opcional, divide sitemaps si tenés muchas páginas
+};
