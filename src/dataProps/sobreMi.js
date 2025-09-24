cafced81eb0f229dbc14a9406c96fd93b492fd4b
Christian <christian_96_09@hotmail.com>
2025-09-24 15:35:38 -0300

    actualiza subtitulo y mejora descripcion en sobreMi.js

--- src/dataProps/sobreMi.js
+++ src/dataProps/sobreMi.js
@@ -1,10 +1,12 @@
 export const sobreMi = {
   titulo: "Hola, soy Christian",
-  subtitulo: "Desarrollador Full Stack",
+  subtitulo: "Desarrollador Web Full Stack",
   descripcion:
-`Soy un apasionado por construir soluciones tecnológicas que generen impacto real en los negocios. Me especializo en integración de sistemas críticos, aplicaciones web escalables y en transformar procesos complejos en herramientas simples y efectivas.
-    
-Tecnologías clave: .NET, C#, React, Laravel, SQL Server, APIs REST, Git
+`Soy un apasionado por construir soluciones tecnológicas que generen impacto real en los negocios. 
 
-Busco desafíos donde aplicar mi mix único de habilidades técnicas + visión de negocio.`,
+Me especializo en integración de sistemas críticos, aplicaciones web escalables que acompañen el crecimiento de tu negocio y transformar procesos complejos en herramientas simples y efectivas.
+
+Tecnologías principales: .NET, C#, React, Laravel, SQL Server, APIs REST, Git
+
+Busco proyectos donde pueda aplicar mi combinación única de habilidades técnicas y visión de negocio.`,
 };
