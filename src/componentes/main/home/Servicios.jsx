68fc4f7a268ca5581ca63117286ea77aaad12718
Christian <christian_96_09@hotmail.com>
2025-09-25 12:21:59 -0300

    actualiza el componente Servicios para incluir iconos en los elementos y mejora el estilo del contenedor

--- src/componentes/main/home/Servicios.jsx
+++ src/componentes/main/home/Servicios.jsx
@@ -1,3 +1,4 @@
+import Link from "next/link"
 import { obtenerIcono } from "../../../helpers/obtenerIcono"
 import styles from './servicios.module.css'
 
@@ -7,7 +8,9 @@ const Servicios = ({servicios}) => {
         <section className={styles.servicios} id="servicios">
             <h2>¿En qué te puedo ayudar?</h2>
 
-            {servicios.map(serv => <ServicioItem serv={serv} key={serv.titulo} />)}
+            <div className={styles.contenedor}>
+                {servicios.map(serv => <ServicioItem serv={serv} key={serv.titulo} />)}
+            </div>
         </section>
 
     )
@@ -17,9 +20,12 @@ export default Servicios
 
 const ServicioItem = ({serv}) => {
     return (<article className={styles.servicioItem}>
+        <i className={serv.icono}></i>
         <h3>{serv.titulo}</h3>
         <p>
             {serv.resumen}
         </p>
+
+        <Link href="/contacto" className="cta">Mas info →</Link>
     </article>)
 }
--- src/dataProps/servicios.ts
+++ src/dataProps/servicios.ts
@@ -2,7 +2,7 @@
 interface servicio {  
   titulo:string,
   resumen:string,
-  descripcion:string,
+  icono:string
   
 }
 
@@ -15,22 +15,24 @@ Permiten que tu negocio tenga presencia en línea y llegue a más clientes.
 
 Hosting y dominio incluidos.
     `,
-    descripcion: "",
+    icono: "fa-brands fa-react"
   },
+
   {  
     titulo: "Aplicaciones Empresariales",
     resumen: `Desarrollo de software para optimizar procesos internos.
     
 Podés gestionar stock, turnos, facturación, clientes y más.
     `,
-    descripcion: "",
+    icono: "fa-solid fa-gear"
   },
+
   {
     titulo: "Automatización e Inteligencia Artificial",
     resumen: `Implementación de soluciones de IA.
     
 Automatización para mejorar la eficiencia y reducir tiempo gastado en tareas repetitivas.    
     `,
-    descripcion: "",
+    icono: "fa-solid fa-robot"
   },
 ];
